9ef7e11f5aa042535aad1ee2de83dc5b43a47a7c
sara190420 <135627303+sara190420@users.noreply.github.com>
2023-06-06 16:37:50 +0200

    Update and rename vjezba1.cs to Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+   
+    class Program
+        
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Vjezba1!");
+            Console.WriteLine("Vjezba122!");
+           Console.WriteLine("Novo auau ");
+        }
+    }
+}
